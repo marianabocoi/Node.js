6944ba7240cfe70da98218b2729f68d141e8eb1c
Mariana <mariana.bocoi@gmail.com>
2019-08-23 19:32:59 +0200

    Adds esample simple http server

---
+++ week1/examples/simple_http/index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+
+//create a server object:
+function createServer() {
+  http
+    .createServer(function(req, res) {
+      switch (req.url) {
+        case "/hello":
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.write(JSON.stringify({ greeting: "Hello World!" })); //write a response to the client
+          break;
+        default:
+          res.writeHead(404, { "Content-Type": "application/json" });
+          res.write(JSON.stringify({ error: "Error" })); //write a response to the client
+      }
+      res.end(); //end the response
+    })
+    .listen(8080); //the server object listens on port 8080
+  console.log("Sertver started on: http://localhost:8080/");
+}
+
+createServer();
