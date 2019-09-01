309ecbd9ec34c287b98eef3039e4db47fa3bf2ae
Mariana <mariana.bocoi@gmail.com>
2019-09-01 13:47:09 +0200

    split the data in the array

--- week2/example/index.js
+++ week2/example/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const name = process.argv[2]
 
 function hello(name = 'you') {
@@ -8,3 +10,11 @@ function hello(name = 'you') {
 hello(name)
 const randomNumber = Math.floor(Math.random() * 200) + 1
 console.log(randomNumber)
+
+fs.readFile('words.txt', 'utf8', function (err, data) {
+  if (err) throw error;
+  console.log('It worked!')
+  // console.log(data)
+  const wordsArray = data.split('\n')
+  console.log(wordsArray)
+})
