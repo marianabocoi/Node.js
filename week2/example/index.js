ff117ace1d64699c62c535ff0b0392963b160b27
Mariana <mariana.bocoi@gmail.com>
2019-09-01 05:53:09 +0200

    4. lists first 10 words

--- week2/example/index.js
+++ week2/example/index.js
@@ -1,13 +1,34 @@
 // run with 
 // npm start Mariana
 
+const fs = require('fs');
+
+const DEFAULT_ENCODING = 'utf8';
+const STORE_FILE_NAME = 'words.txt';
+
 function getNumber() {
   return Math.floor(Math.random() * 200) + 1
+}
+
+function readFile() {
+  return new Promise(
+    resolve => fs.readFile(
+      STORE_FILE_NAME,
+      DEFAULT_ENCODING,
+      (err, data) => resolve(err ? '' : data)
+    )
+  );
 }
 
 function hello(name) {
   console.log(`Hello, ${name}!`)
   console.log(getNumber())
+  readFile().then(data => {
+    words = data.split(/\r?\n/);
+    for (let index = 0; index < 10; index++) {
+      console.log(words[index])
+    }
+  })
 }
 
 const name = process.argv[2];
