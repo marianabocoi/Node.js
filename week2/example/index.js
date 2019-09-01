d7f5dfe773e84ddfeafe60e92824d55a1ea1ba80
Mariana <mariana.bocoi@gmail.com>
2019-09-01 14:41:52 +0200

    we made it save a file with a random word

--- week2/example/index.js
+++ week2/example/index.js
@@ -6,10 +6,12 @@ function hello(name = 'you') {
 
   console.log(`Hello, ${name}`)
 }
+let randomWord = 'test'
 
 hello(name)
 const randomNumber = Math.floor(Math.random() * 200) + 1
 console.log(randomNumber)
+
 
 fs.readFile('words.txt', 'utf8', function (err, data) {
   if (err) throw error;
@@ -17,6 +19,12 @@ fs.readFile('words.txt', 'utf8', function (err, data) {
   // console.log(data)
   const wordsArray = data.split('\n')
   // console.log(wordsArray)
-  const firstFiveWords = wordsArray.splice(0, 5)
-  console.log(firstFiveWords)
-})
+  // const firstFiveWords = wordsArray.splice(0, 5)
+  randomWord = wordsArray[randomNumber - 1]
+  console.log(randomWord)
+
+  fs.writeFile('randomWord.txt', randomWord, function (err) {
+    if (err) throw error;
+    console.log('Success!')
+  })
+}) 
